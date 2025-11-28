5f9ffbf6e54acbfee8c04ef9c599312bbb22fd9d
USFrozen <frozen@coldlab.me>
2025-11-27 23:11:49 -0600

    moved collision and transition sprites to sprites.py, added draw layer to entitiy class

--- sprites.py
+++ sprites.py
@@ -1,15 +1,16 @@
 from settings import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, group):
+    def __init__(self, pos, surf, group, layer = WORLD_DRAW_ORDER['main']):
         super().__init__(group)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+        self.draw_layer = layer
 
 class AnimatedSprite(Sprite):
-    def __init__(self, pos, frames, group):
+    def __init__(self, pos, frames, group, layer = WORLD_DRAW_ORDER['main']):
         self.frame_index, self.frames = 0, frames
-        super().__init__(pos, frames[self.frame_index], group)
+        super().__init__(pos, frames[self.frame_index], group, layer)
 
     def animate(self, dt):
         self.frame_index += ANIMATION_SPEED * dt
@@ -20,13 +21,41 @@ class AnimatedSprite(Sprite):
 
 
 class TransitionSprite(Sprite):
-    def __init__(self, pos, size, target_map, current_map, group):
+    def __init__(self, pos, size, target_map, current_map, group, layer = WORLD_DRAW_ORDER['main']):
         surf = pygame.Surface(size)
-        super().__init__(pos, surf, group)
+        super().__init__(pos, surf, group, layer)
         self.target_map = target_map
         self.current_map = current_map
 
 class CollisionSprite(Sprite):
-    def __init__(self, pos, size, group):
+    def __init__(self, pos, size, group, layer = WORLD_DRAW_ORDER['main']):
         surf = pygame.Surface(size)
-        super().__init__(pos, surf, group)
+        super().__init__(pos, surf, group, layer)
+
+# From .mtx Monsters layer, environment objects where monsters can appear
+class MonsterEnv(pygame.sprite.Sprite):
+    def __init__(self, pos, group, obj, tmx_data, layer = WORLD_DRAW_ORDER['main']):
+        super().__init__(group)
+
+        if obj.gid:
+            self.image = tmx_data.get_tile_image_by_gid(obj.gid)
+        else:
+            self.image = pygame.Surface((16, 16))
+            self.image.fill('magenta')
+
+        self.rect = self.image.get_frect(topleft=pos)
+        self.draw_layer = layer
+
+# From .mtx Entities layer, used for objects that can be interacted with
+class MapObject(pygame.sprite.Sprite):
+    def __init__(self, pos, group, obj, tmx_data, layer = WORLD_DRAW_ORDER['main']):
+        super().__init__(group)
+
+        if obj.gid:
+            self.image = tmx_data.get_tile_image_by_gid(obj.gid)
+        else:
+            self.image = pygame.Surface((16, 16))
+            self.image.fill('magenta')
+
+        self.rect = self.image.get_frect(topleft=pos)
+        self.draw_layer = layer
