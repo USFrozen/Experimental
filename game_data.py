cde62e38ff93a94caefa29cd54e653db8288eb28
USFrozen <frozen@coldlab.me>
2025-12-01 17:21:50 -0600

    changed NPC text slightly

--- game_data.py
+++ game_data.py
@@ -1,7 +1,7 @@
 NPC_DATA = {
     '0001': {
         'dialogue': {
-            'default': ['Hello, my name is Professor Barry Griffiths!', 'I never thought id be an NPC in a Pokemon knockoff!']
+            'default': ["Hello, my name is Professor Griffiths.", "I never thought i'd be an NPC in a Pokemon knockoff!"]
         },
         'directions': ['down', 'left', 'up', 'right'],
         'look_around': False,
