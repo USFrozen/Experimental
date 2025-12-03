9733f2fc33f25021e0b0e53de1555aa1cad1b22e
USFrozen <frozen@coldlab.me>
2025-12-03 15:28:52 -0600

    added timer class to helpers for use in battle and other random stuff

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 from settings import *
+from pygame.time import get_ticks
 
 # Import Functions
 # Used for importing single image files
@@ -110,3 +111,31 @@ def check_connections(distance, entity, target, tollerance = 16):
             entity.facing_direction == 'down' and relationship.y > 0 and abs(relationship.x) < tollerance
         ):
             return True
+
+# Timer for use in battle system
+class Timer:
+    def __init__(self, duration, repeat = False, autostart = False, func = None):
+        self.duration = duration
+        self.start_time = 0
+        self.active = False
+        self.repeat = repeat
+        self.func = func
+        if autostart:
+            self.activate()
+
+    def activate(self):
+        self.active = True
+        self.start_time = get_ticks()
+
+    def deactivate(self):
+        self.active = False
+        self.start_time = 0
+        if self.repeat:
+            self.activate()
+
+    def update(self):
+        if self.active:
+            current_time = get_ticks()
+            if current_time - self.start_time >= self.duration:
+                if self.func: self.func()
+                self.deactivate()
